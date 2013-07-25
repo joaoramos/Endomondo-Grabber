207aae36fc6e27da8a71229dcf0ae4aead34b5af
João Ramos <hello@joao.pt>
2013-07-25 18:01:00 +0200

    Deleted personal user ID

--- endomondo.php
+++ endomondo.php
@@ -1,25 +1,25 @@
 <?php
-    //VARIABLES -- EDIT THIS VALUES
-    $userID = '2226023'; //THIS IS FOR JOÃO RAMOS! PUT YOUR OWN IN HERE
+
+    $userID = ''; // User ID
     $sport = '0';
-    /* Edit this if you want to change the sport
-     * 0 - Running
-     * 1 - Cycling, transport
-     * 2 - Cycling, sport
-     * 3 - Mountain Biking
-     * 4 - Skating
-     * 5 - Roller Skiing
-     * 6 - Skiing, cross country
-     * 7 - Skiing, downhill
-     * 8 - Snowboarding
-     * ...
-     * 94 - Treadmill walking
-     */
+    /*
+        0 - Running
+        1 - Cycling, transport
+        2 - Cycling, sport
+        3 - Mountain Biking
+        4 - Skating
+        5 - Roller Skiing
+        6 - Skiing, cross country
+        7 - Skiing, downhill
+        8 - Snowboarding
+        ...
+        94 - Treadmill walking
+    */
     
-    //GET DATE
-    $to = date('Ymd'); //Today's date
-    $from = date('Ymd', strToTime('-6 days')); //One week ago
-    // <!-- DATE -->
+    // Get date (last 7 days)
+    $to = date('Ymd'); // Today's date
+    $from = date('Ymd', strToTime('-6 days')); // Last week
+
     function GetBetween($content,$start,$end){
         $r = explode($start, $content);
         if (isset($r[1])){
@@ -28,12 +28,13 @@
         }
         return '';
     }
-    //GET CONTENTS
+
+    // Get contents
     $data = file_get_contents('http://www.endomondo.com/embed/user/summary?id='.$userID.'&sport='.$sport.'&from='.$from.'&to='.$to.'&measure=0&zone=Gm0000_GMT&width=680&height=217');
     $distance = strip_tags(GetBetween($data, '<th>Distance:</th>', '</td>'));
     $duration = strip_tags(GetBetween($data, '<th>Duration:</th>', '</td>'));
 
-    //PRINT TO FILES
+    // Print to files
     $fp = fopen('workout-duration.txt', 'w');
     fwrite($fp, $duration);
     fclose($fp);
